cd1e38b3e6ee8684ed1f96d8dee188b5a96d8c36
Hoàng Kim Long <longhoang8204@gmail.com>
2024-02-24 04:23:59 +0700

    Update page 2

---
+++ resources/views/partials/banner.blade.php
@@ -0,0 +1,11 @@
+<div class="banner">
+  <div class="row">
+    <div class="col-6">
+
+    </div>
+    <div class="col-6">
+      <img src="/images/brand.png" alt="brand" srcset="">
+    </div>
+
+  </div>
+</div>
--- resources/views/partials/intro.blade.php
+++ resources/views/partials/intro.blade.php
@@ -3,36 +3,46 @@
     <div class="background">
       <img src="/images/intro-sticky.svg" alt="">
     </div>
-    <div class="container intro">
-      <div class="intro-text">
-        <h3>Chẳng có ai thích cái gì XẤU cả,<br>
-          KHÁCH HÀNG CỦA BẠN lại càng không!</h3>
-        <h1>Hãy để 😀 VUI VẺ<br>
-          Thiết kế Website <span class="border-1">THẬT ĐẸP</span><br>
-          <span class="border-2">TẠO THƯƠNG HIỆU</span> cho BẠN!
-        </h1>
-      </div>
-      <div class="customer-contact-form">
-        <div class="customer-contact-form-header">
-          <h3>Cùng Vui Vẻ thiết kế Website của bạn, tạo ra thương hiệu cho chính mình</h3>
+    <div class="container intro d-flex justify-content-around">
+      <div class="row d-flex justify-content-around">
+        <div class="col-6 d-flex flex-column justify-content-around">
+          <div class="intro-text">
+            <h3>Chẳng có ai thích cái gì XẤU cả,<br>
+              KHÁCH HÀNG CỦA BẠN lại càng không!</h3>
+            <h1>Hãy để 😀 VUI VẺ<br>
+              Thiết kế Website <span class="border-1">THẬT ĐẸP</span><br>
+              <span class="border-2">TẠO THƯƠNG HIỆU</span> riêng BẠN!
+            </h1>
+          </div>
+          <div class="intro-video">
+            <video class="img" width="100%" autoplay="" loop="" muted="" playsinline="" __idm_id__="245761">
+              <source src="https://mona.media/template/assets/images/tkw-never-stop/Website.webm" type="video/webm">
+              <source src="https://mona.media/template/assets/images/tkw-never-stop/Website.mp4" type="video/mp4">
+            </video>
+          </div>
         </div>
-        <div class="customer-contact-form-body">
-          <input type="text" name="name" id="c-name" placeholder="Họ và tên">
-          <input type="text" name="company" id="c-company" placeholder="Tên doanh nghiệp (không bắt buộc)">
-          <input type="text" name="phone" id="c-phone" placeholder="Số điện thoại">
-          <input type="email" name="email" id="c-email" placeholder="Email">
-          <input type="text" name="domain" id="c-domain" placeholder="Tên miền (không bắt buộc)">
-        </div>
-        <div class="customer-contact-form-footer">
-          <button class="btn" id="customer-contact-btn">
-            <div class="btn-img">
-              <img src="/images/bolt.gif" alt="bolt">
-            </div>
-            <div class="btn-text">
-              <h4>Yêu cầu tư vấn MIỄN PHÍ</h4>
-              <p>VUI VẺ sẽ trả lời bạn trong vòng <span style="font-weight: bold">30p</span> làm việc</p>
-            </div>
-          </button>
+        <div class="customer-contact-form col-6 d-flex align-self-center">
+          <div class="customer-contact-form-header">
+            <h3>Cùng Vui Vẻ thiết kế Website của bạn, tạo ra thương hiệu cho chính mình</h3>
+          </div>
+          <div class="customer-contact-form-body">
+            <input type="text" name="name" id="c-name" placeholder="Họ và tên">
+            <input type="text" name="company" id="c-company" placeholder="Tên doanh nghiệp (không bắt buộc)">
+            <input type="text" name="phone" id="c-phone" placeholder="Số điện thoại">
+            <input type="email" name="email" id="c-email" placeholder="Email">
+            <input type="text" name="domain" id="c-domain" placeholder="Tên miền (không bắt buộc)">
+          </div>
+          <div class="customer-contact-form-footer">
+            <button class="btn" id="customer-contact-btn">
+              <div class="btn-img">
+                <img src="/images/contact.gif" alt="bolt">
+              </div>
+              <div class="btn-text">
+                <h4>Yêu cầu tư vấn <b>MIỄN PHÍ</b></h4>
+                <p>VUI VẺ sẽ trả lời bạn trong vòng <span style="font-weight: bold">30p</span> làm việc</p>
+              </div>
+            </button>
+          </div>
         </div>
       </div>
     </div>
